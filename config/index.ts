17ba1bfa18d900ef243fd5a1dc23fde5cdab9682
shubham kumar <theshubham.dev@gmail.com>
2024-09-13 17:24:59 +0530

    setup:migrate-to-cli

--- config/index.ts
+++ config/index.ts
@@ -1,11 +1,18 @@
-const apiKey = process.env.EXPO_PUBLIC_apiKey || "";
-const authDomain = process.env.EXPO_PUBLIC_authDomain || "";
-const projectId = process.env.EXPO_PUBLIC_projectId || "";
-const storageBucket = process.env.EXPO_PUBLIC_storageBucket || "";
-const messagingSenderId = process.env.EXPO_PUBLIC_messagingSenderId || "";
-const appId = process.env.EXPO_PUBLIC_appId || "";
-const env = process.env.ENV || "dev";
+// const apiKey = process.env.EXPO_PUBLIC_apiKey || "";
+// const authDomain = process.env.EXPO_PUBLIC_authDomain || "";
+// const projectId = process.env.EXPO_PUBLIC_projectId || "";
+// const storageBucket = process.env.EXPO_PUBLIC_storageBucket || "";
+// const messagingSenderId = process.env.EXPO_PUBLIC_messagingSenderId || "";
+// const appId = process.env.EXPO_PUBLIC_appId || "";
+const env = process.env.ENV || 'dev';
 
+const apiKey = 'AIzaSyD0rcoVpsAKoTmjnFhAM6PqkxX5I1x6Rzc';
+const authDomain = 'easysplit-1a0d6.firebaseapp.com';
+const projectId = 'easysplit-1a0d6';
+const storageBucket = 'easysplit-1a0d6.appspot.com';
+const messagingSenderId = '291038701878';
+const appId = '1=291038701878=web=27d54286ad13077ae7eb97';
+const measurementId = 'G-JPR3D02WD4';
 export {
   env,
   apiKey,
